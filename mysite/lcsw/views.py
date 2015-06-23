a44435a0ffddb9e0293e2b9d21b2c2d8465ed8cf
Jim Lin <jim_lin@quantatw.com>
2015-06-23 13:22:53 +0800

    rm *.pyc

--- mysite/lcsw/views.py
+++ mysite/lcsw/views.py
@@ -12,4 +12,4 @@ def home(request):
 #        "It works on my machine",
 #    ]
     excuse = random.choice(Excuse.objects.all())
-    return render(request, "index.html", {'excuse': excuse})
+    return render(request, "index.html", {'excuse': excuse.content})
